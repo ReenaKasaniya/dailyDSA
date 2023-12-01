036152e3747d81763db39a50441233e8266eaf4b
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-01 22:27:38 +0530

    Program if the two numbers have same last digit

---
+++ 1.js
@@ -0,0 +1,26 @@
+17; // Program to check if the two number have Same last Digit
+
+// way1
+// const prompt = require("prompt-sync")();
+// const num1 = parseInt(prompt("enter the  first number : "));
+// const num2 = parseInt(prompt("enter the second number : "));
+// LD1 = num1 % 10;
+// LD2 = num2 % 10;
+
+// if (LD1 === LD2) {
+//   console.log(" Number have Same last digit ");
+// } else {
+//   console.log(" Number don't have Same last digit ");
+// }
+// way2
+// const prompt = require("prompt-sync")();
+// const num1 = parseInt(prompt("enter the  first number : "));
+// const num2 = parseInt(prompt("enter the second number : "));
+// LD1 = num1 % 10;
+// LD2 = num2 % 10;
+
+// if (LD1 === LD2) {
+//   console.log(" Number have Same last digit ");
+// } else {
+//   console.log(" Number don't have Same last digit ");
+// }
