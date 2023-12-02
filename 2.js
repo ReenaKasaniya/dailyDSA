a46061ff0a3dc7163ddb3cbe74708de340572a31
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-02 22:15:38 +0530

    Program to find the LCM of the given Number

---
+++ 2.js
@@ -0,0 +1,15 @@
+// Program to find the LCM of the given Number
+
+// const prompt = require("prompt-sync")();
+// const num1 = prompt("Enter a first number: ");
+// const num2 = prompt("Enter a second number: ");
+
+// let min = num1 > num2 ? num1 : num2;
+
+// while (true) {
+//   if (min % num1 == 0 && min % num2 == 0) {
+//     console.log(`The LCM of ${num1} and ${num2} is ${min}`);
+//     break;
+//   }
+//   min++;
+// }
