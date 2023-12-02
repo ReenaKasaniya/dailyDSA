f0e5c5e140e22c9b61cbd211a1ac5d6d17e06d2b
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-02 22:32:13 +0530

    Program to find factors of a number

---
+++ 3.js
@@ -0,0 +1,11 @@
+// Program to find factors of a number
+
+// const prompt = require("prompt-sync")();
+// const num = parseInt(prompt("Enter the Number : "));
+
+// console.log(`The factors of the ${num} is `);
+// for (i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     console.log(i);
+//   }
+// }
