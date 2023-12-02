677c7242253a761b69932f9887d92fbfe130c868
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-02 22:49:46 +0530

    Program to find HCF & GCD of given number

---
+++ 4.js
@@ -0,0 +1,13 @@
+//24.. Program to find HCF & GCD of given number
+
+// const prompt = require("prompt-sync")();
+// let hcf;
+// const num1 = prompt("Enter a first positive integer: ");
+// const num2 = prompt("Enter a second positive integer: ");
+
+// for (let i = 1; i <= num1 && i <= num2; i++) {
+//   if (num1 % i == 0 && num2 % i == 0) {
+//     hcf = i;
+//   }
+// }
+// console.log(`HCF of ${num1} and ${num2} = ${hcf}`);
