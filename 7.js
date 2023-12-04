913fa571e435bd9659f89ae7888498d9faae1268
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-04 23:56:51 +0530

    to find the missing number in series in an arrray

---
+++ 7.js
@@ -0,0 +1,8 @@
+// to find the missing number in series in an arrray
+// const A = [1, 2, 4];
+// const N = 4;
+// let number = (N * (N + 1)) / 2;
+// for (let i = 0; i < N - 1; i++) {
+//   number -= A[i];
+// }
+// console.log(number);
