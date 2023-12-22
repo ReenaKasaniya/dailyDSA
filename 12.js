744b12b701316c5b1fd73b50ebb22d03d7bffa5c
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-22 22:41:12 +0530

    Find the largest number among three number

---
+++ 12.js
@@ -0,0 +1,15 @@
+//05  Find the largest number among three number
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if (a > b && a > c) {
+//   console.log(`Largest Number is a ${a}`);
+// } else if (b > a && b > c) {
+//   console.log(`Largest Number is a ${b}`);
+// } else if (c > a && c > b) {
+//   console.log(`Largest Number is a ${c}`);
+// } else {
+//   return 0;
+// }
