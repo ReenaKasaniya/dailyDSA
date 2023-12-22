4e385e73010f7fd67de1db66b66fa89ee02cd4c6
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-22 22:39:33 +0530

    program to convert kilometer to miles

---
+++ 13.js
@@ -0,0 +1,12 @@
+// program to convert kilometer to miles
+
+// function myFunction(p1) {
+//     return p1 * 0.621371;
+//   }
+
+//   console.log(myFunction(2));
+
+// const kilometers = prompt("Enter value in kilometers: ")
+// const factor = 0.621371
+// const miles = kilometers * factor
+// console.log(`${kilometers} kilometers is equal to ${miles} miles. `)
