ea36c80637726c6d1c634cd1c6e821f44e2b41cd
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-23 23:08:07 +0530

    program to find square root of two numbers

---
+++ 14.js
@@ -0,0 +1,13 @@
+//07 program to find square root of two numbers
+
+// function squareroot(number) {
+//     var number;
+//     for (var i = number; i >= 1; i--) {
+//         if (i * i === number) {
+//             number = i;
+//             break;
+//        }
+//    }
+//    return number;
+// }
+//  console.log(squareroot(64))
