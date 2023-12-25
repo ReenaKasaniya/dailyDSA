34309b56ad941fb8e82ee9a3918165eeea1e14ba
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-25 22:21:14 +0530

    check if a given number is positive negative or zero

---
+++ 16.js
@@ -0,0 +1,12 @@
+//09 Program to check if a given number is positive negative or zero
+
+// let num= 0;
+
+// if (num<0){
+// console.log(`The given Number ${num} is Negative`)
+// }
+// else if (num>0){
+//     console.log(`The given Number ${num} is Positive`)
+// }
+// else{
+// console.log("The given number is zero")}
