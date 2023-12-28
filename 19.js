3d340bb966b6e9a9a24c28afa332fd7f76d04e0b
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-28 22:01:22 +0530

    Program to display multiplication table

---
+++ 19.js
@@ -0,0 +1,8 @@
+// 12,15 Program to display multiplication table
+
+// let num = 3;
+// let num1;
+// for (i = 1; i <= 10; i++) {
+//   num1 = num * i;
+//   console.log(`${num} x ${i} = ${num1}`);
+// }
