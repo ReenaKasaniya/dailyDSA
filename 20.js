5baff2198de54fe942a5a10632e5f458b876548c
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2023-12-29 22:27:31 +0530

    program to find factorial of a number

---
+++ 20.js
@@ -0,0 +1,19 @@
+// 14 program to find factorial of a number
+
+// let n = -5;
+// function factorial(n) {
+//   let product = 1;
+//   if (n == 0 || n == 1) {
+//     return product;
+//   } else if (n > 1) {
+//     for (i = n; i >= 1; i--) {
+//       product = product * i;
+//     }
+//     return product;
+//   } else {
+//     return "number has to be positive";
+//   }
+// }
+
+// product = factorial(n);
+// console.log(`factorial of ${n} is ${product}`);
