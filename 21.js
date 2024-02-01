106e7dc4d5050a24ed239186b5b209ca2851f8ed
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2024-02-01 22:41:05 +0530

    Program to print all prime number in an interval

---
+++ 21.js
@@ -0,0 +1,25 @@
+// 13 Program to print all prime number in an interval
+
+// const prompt = require("prompt-sync")();
+// const num1 = parseInt(prompt("enter the lower number"));
+// const num2 = parseInt(prompt("enter the upper number"));
+// console.log(num1);
+// console.log(num2);
+// let n = 15;
+
+// function prime(n) {
+//   if (n === 1) {
+//     console.log(" 1 is neigther prime nor composite number");
+//   } else if (i > 1) {
+//     for (i = 2; i < n; i++) {
+//       if (n % i == 0) {
+//         console.log("the given number is not a prime number");
+//       } else {
+//         console.log("the given number is prime number");
+//         break;
+//       }
+//     }
+//   }
+// }
+
+// prime(n);
