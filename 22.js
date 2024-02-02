16579ebc792a05077b848558c56ae03c9d21cffb
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2024-02-02 13:31:16 +0530

    Program to create multiline string

---
+++ 22.js
@@ -0,0 +1,16 @@
+// 50.. Program to create multiline string
+
+// way 1
+// let multiLine = `Hello Everyone
+// here we are going to learn multiline string
+// Hope you are ready to learn this.
+// with full energy..`;
+
+// way 2
+// let multiLine =
+//   ` Hello Everyone.\n ` +
+//   `here we are going to learn multiline string.\n ` +
+//   `Hope you are ready to learn this.\n ` +
+//   `with full energy.\n `;
+
+// console.log(multiLine);
