5c6e509e5c9fc4d61adf9722ef219dd77830708a
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2024-02-03 11:06:01 +0530

    Program to replace all ocurrance of a string

---
+++ 23.js
@@ -0,0 +1,16 @@
+// Program to replace all ocurrance of a string
+
+// way1
+// let Str =
+//   "STRING is a Core Data Resource as designated by Global Biodata Coalition and ELIXIR.";
+
+// const newStr = Str.replaceAll(" ", " + ");
+// console.log(newStr);
+
+// way2
+// const myString = "Apples pears apples strawberries";
+// const pattern = /apples/gi;
+// const replacement = "grapes";
+
+// const result = myString.replaceAll(pattern, replacement);
+// console.log(result);
