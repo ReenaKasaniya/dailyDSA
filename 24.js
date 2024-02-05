0053f1d5885b61ef50ba198946e18174d4e1fe64
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2024-02-05 22:37:19 +0530

    JS Program to Add Key/Value Pair to an Object

---
+++ 24.js
@@ -0,0 +1,19 @@
+// JavaScript Program to Add Key/Value Pair to an Object
+
+// way 1
+// const student = { name: "Reena" };
+// student.age = 25;
+// student.village = "Shamgarh";
+// student.dist = "Mandsaur";
+// console.log(student);
+
+// way 2
+// const obj = { name: "Ben" };
+// const prop1 = "age";
+// const prop2 = "village";
+// const prop3 = "Contact no.";
+// obj[prop1] = "22";
+// obj[prop2] = "Garoth";
+// obj[prop3] = "1122334455";
+
+// console.log(obj);
