ed97c1e6cd3fc2cb002217703b74f38d4f18e53d
ReenaKasaniya <125480830+ReenaKasaniya@users.noreply.github.com>
2024-02-06 19:35:52 +0530

    Program to Sort Words in Alphabetical Order

---
+++ 25.js
@@ -0,0 +1,14 @@
+// Program to Sort Words in Alphabetical Order
+
+// const prompt = require("prompt-sync")();
+// const string = prompt("Enter a sentence: ");
+
+// const words = string.split(" ");
+
+// words.sort();
+
+// console.log("The sorted words are:");
+
+// for (const element of words) {
+//   console.log(element);
+// }
